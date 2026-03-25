d1d8ab4d948e9cc428dd257c5d55985abba856a2
EyadEhab <23-101054@students.eui.edu.eg>
2026-03-25 16:57:50 +0200

    removed decorator.report

--- Master dcd java/src/decorator/report/ReportContent.java
+++
@@ -1,9 +0,0 @@
-package decorator.report;
-
-/**
- * Component in the Decorator chain for building report body content.
- */
-public interface ReportContent {
-    String render(ReportContext ctx);
-}
-
